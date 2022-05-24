27e0b051b046b42e325f0ac7441569152c321864
PC <168169332@qq.com>
2022-05-24 17:24:26 +0800

    commit

---
+++ simple/src/main/java/cn/cqray/demo/App.java
@@ -0,0 +1,11 @@
+package cn.cqray.demo;
+
+import cn.cqray.java.util.EmptyUtils;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println(EmptyUtils.check("6666"));
+    }
+}
---
+++ src/test/java/cn/cqray/test/App.java
@@ -0,0 +1,9 @@
+package cn.cqray.test;
+
+import cn.cqray.java.util.StringUtils;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.isEmpty("44"));
+    }
+}
