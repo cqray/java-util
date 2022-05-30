5a7c94773b381e130e69fbd0bc02f29f50734914
Cqray <Cqrqy@189.cn>
2022-05-30 23:09:56 +0800

    commit

--- src/main/java/cn/cqray/java/type/TypeAdapter.java
+++ src/main/java/cn/cqray/java/type/TypeAdapter.java
@@ -17,7 +17,7 @@ public abstract class TypeAdapter<T> {
     }
 
     public final void traversal(Object data, boolean reserve, TraverseCallback<?> callback) {
-        onTraversal((T) data, reserve, (TraverseCallback<T>) callback);
+        onTraversal((T) data, reserve, callback);
     }
 
     /**
@@ -39,5 +39,5 @@ public abstract class TypeAdapter<T> {
      * @param reserve 是否倒序
      * @param callback 回调
      */
-    public abstract void onTraversal(T data, boolean reserve, TraverseCallback<T> callback);
+    public abstract void onTraversal(T data, boolean reserve, TraverseCallback<?> callback);
 }
